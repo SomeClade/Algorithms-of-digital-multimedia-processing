4bcb1af2f1cdb0b6c0a00c9e0fb68c88297cbd77
SomeClade <114222693+SomeClade@users.noreply.github.com>
2024-09-07 15:57:41 +0300

    feat: copy rgb to hsv from lab 1

--- lab2_TrackingRedObject/trackingRedObject.py
+++ lab2_TrackingRedObject/trackingRedObject.py
@@ -1,1 +1,28 @@
 import cv2
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Не удалось открыть камеру")
+    exit()
+
+while True:
+    # Захват кадра с камеры
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Ошибка при получении кадра")
+        break
+
+    # Преобразование изображения в цветовое пространство HSV
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+
+    cv2.imshow('Original Image', frame)
+    cv2.imshow('HSV Image', hsv_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Освобождение ресурсов
+cap.release()
+cv2.destroyAllWindows()
